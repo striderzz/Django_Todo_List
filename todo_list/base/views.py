6a584ba71738336c8d0ff2c9477666c3b5186753
Hasan <hasan98209@gmail.com>
2025-03-09 04:38:33 -0500

    added login and logout

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -5,33 +5,53 @@ from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
 from .models import Task
+
+class CustomLoginView(LoginView):
+  template_name = 'base/login.html'
+  fields = '__all__'
+  redirect_authenticated_user = True
+
+  def get_success_url(self):
+    return reverse_lazy('tasks')
 
 # Create your views here.
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
   model = Task 
   context_object_name = "tasks"
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
   model = Task
   context_object_name = "task"  
   template_name = 'base/task.html'
 
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
   model = Task
   fields = '__all__'
   success_url = reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
   model = Task
   fields = '__all__'
   success_url = reverse_lazy('tasks')  
 
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin, DeleteView):
   model = Task
   context_object_name = 'task' 
   success_url = reverse_lazy('tasks') 
+
+
+def logout_user(request):
+    logout(request)
+
+    return redirect('login')  
 
   
